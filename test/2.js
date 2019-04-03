9a7950631bf398f176dd09b9d1eb0a7f01343e81
thrilling_gem <4096void@gmail.com>
2019-04-03 14:26:26 +0800

    fix typo

--- test/2.js
+++ test/2.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
 const two = require('../2');
 
+// 5
 const five = {
   val: 5,
   next: null,
 };
+// 0 -> 1
 const ten = {
   val: 0,
   next: {
@@ -15,7 +17,7 @@ const ten = {
 
 describe('https://leetcode.com/problems/add-two-numbers/', function () {
   describe('#addTwoNumbers', function () {
-    it('should return [0, 1] when addTwoNumbers([5], [5])', function () {
+    it('should return 0 -> 1 when addTwoNumbers(5, 5)', function () {
       assert.deepEqual(two.addTwoNumbers(five, five), ten);
     });
   });
