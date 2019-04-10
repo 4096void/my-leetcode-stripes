a23dff849b9d8de261127ea2b6060fa042b8a33d
thrilling_gem <4096void@gmail.com>
2019-04-10 11:44:53 +0800

    add #1 solution 2

--- 1.js
+++ 1.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 
+// solution 1 112ms 51.67% 34.7MB 51.95%
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -15,5 +16,16 @@ var twoSum = function (nums, target) {
 
   return [];
 }
+
+// solution 2 180ms 17.31% 33.9MB 96.94%
+var twoSum_v2 = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let idx = nums.indexOf(target - nums[i]);
+    if (idx > -1 && idx !== i) {
+      return [i, idx];
+    }
+  }
+  return []
+};
 
 exports.twoSum = twoSum;
